21cbef46a1f4b7a46d669a152e2e13cfb3718f81
Roy Anger <roy@royanger.com>
2022-07-20 17:54:46 -0400

    Added basic icons to get started

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,21 @@
+import AssignIcon from './AssignIcon';
+import CalendarIcon from './CalendarIcon';
+import CheckmarkIcon from './CheckmarkIcon';
+import EditIcon from './EditIcon';
+import LogoutIcon from './LogoutIcon';
+import MinusIcon from './MinusIcon';
+import PlusIcon from './PlusIcon';
+import StarIcon from './StarIcon';
+import UploadIcon from './UploadIcon';
+
+export {
+	AssignIcon,
+	CalendarIcon,
+	CheckmarkIcon,
+	EditIcon,
+	LogoutIcon,
+	MinusIcon,
+	PlusIcon,
+	StarIcon,
+	UploadIcon,
+};
