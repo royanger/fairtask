bf7a0b3a0963ef8ded71cbc0bb60b9b099b84283
Roy Anger <roy@royanger.com>
2022-07-20 02:36:03 -0400

    Added base colour palette to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,32 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
 		fontFamily: {
 			poppins: ['Poppins', 'sans-serif'],
 			sfprodisplay: ['SF Pro Display', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				green: {
+					200: '#B1C4C4',
+					300: '#83A1A1',
+					DEFAULT: '#324646',
+					500: '#324646',
+				},
+				blue: {
+					DEFAULT: '#5966DB',
+				},
+				black: {
+					DEFAULT: '#000000',
+				},
+				background: {
+					DEFAULT: '#F5F5F5',
+				},
+				grey: {
+					100: '#EAEBEC',
+					DEFAULT: '#D1D1D6',
+				},
+			},
 		},
 	},
 	plugins: [],
